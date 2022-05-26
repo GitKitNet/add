bfb99916b96bdbcba999fc68de83d407238b1678
GitKitNet <gitkitnet@gmail.com>
2022-05-26 15:06:10 +0300

    Add OwnCloud
    OwnCloud
    
    Co-Authored-By: numbnet <8426426+numbnet@users.noreply.github.com>

--- cptoolkit.sh
+++ cptoolkit.sh
@@ -57,8 +57,20 @@ BGCOLOR=$BLACK;
 #       ASK START
 #===================================
 
-function THIS() { clear; while true; do echo -en "\t${Yellow}Do you want Run This script [y/N] .?${RC} "; read -e syn; case $syn in [Yy]* )clear; echo -e "\n\t${GREEN}Starting NOW..${NC}"; sleep 3; break;; [Nn]* )exit 0;; esac; done; };
+function THIS() { 
+  clear; 
+  while true; do 
+    echo -en "\t${Yellow}Do you want Run This script [y/N] .?${RC} "; 
+    read -e syn; 
+    case $syn in 
+      [Yy]* ) clear; echo -e "\n\t${GREEN}Starting NOW..${NC}"; sleep 3; break;; 
+      [Nn]* )exit 0;;
+  esac; 
+done;
+};
 THIS
+
+
 
 #===================================
 #      VARIABLE & function
@@ -1932,25 +1944,413 @@ if ( !defined('ABSPATH') )
 
 require_once(ABSPATH . 'wp-settings.php');
 EOL
-
         chown -R $username:$username /var/www/$username
         echo -e "${GREEN}Database user, database and wp-config.php were succesfully created & configured!${NC}"
         sleep 3
-
 }
 
 function installLAMP() {
-        CheckPack;
-        ChangingPMA;
-        creatingUser;
-        ConfApache2;
-        InstallWordPress
-        #downWP;
-        CreatSWAP;
-        creSECURE;
-        AddingDB;
-        echo -e "Installation & configuration succesfully finished.";
-        sleep 3
+  CheckPack;
+  ChangingPMA;
+  creatingUser;
+  ConfApache2;
+  InstallWordPress
+  #downWP;
+  CreatSWAP;
+  creSECURE;
+  AddingDB;
+  echo -e "Installation & configuration succesfully finished.";
+  sleep 3
+}
+
+
+#=================================
+#         OwnCloud
+#=================================
+function OWNCLOUD() {
+
+# ------------------------------
+#         VAR FUNCTION
+# ------------------------------
+
+function wait() { echo -e -n "${YELLOW}Press [ANY] key to continue...${NC}\n"; read -s -n 1; }
+function pause() { read -p "Press [Enter] key to continue..." fackEnterKey; }
+function myip() {
+  ipH=$(hostname -I|cut -f1 -d ' ');
+  ipW="$(echo $(curl -s -4 icanhazip.com))";
+  ipE="$(ip addr show eth0 |grep inet |awk '{ print $2; }' |sed 's/\/.*$//' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' )";
+  if [ "$ipE" == "$ipW" ]; then myip="$ipE"; elif [ "$ipH" == "$ipW" ]; then  myip="$ipH"; else myip="$ipW"; fi; echo "${myip}";
+}
+
+myip=$(hostname -I|cut -f1 -d ' ')
+
+#--------------------------------------
+#      OwnCloud Ubuntu 18
+#--------------------------------------
+function OwnCloud_V18() {
+
+# Preparation
+# ----------------------
+FILE="/usr/local/bin/occ"
+/bin/cat <<EOM >$FILE
+#! /bin/bash
+
+cd /var/www/owncloud
+sudo -u www-data /usr/bin/php /var/www/owncloud/occ "\$@"
+EOM
+
+
+chmod +x /usr/local/bin/occ
+Install the Required Packages
+apt install -y \
+    apache2 \
+    libapache2-mod-php \
+    mariadb-server \
+    openssl \
+    php-imagick php-common php-curl \
+    php-gd php-imap php-intl \
+    php-json php-mbstring php-mysql \
+    php-ssh2 php-xml php-zip \
+    php-apcu php-redis redis-server \
+    wget
+
+# ----------------------
+apt install -y \
+    ssh bzip2 sudo cron rsync curl jq \
+    inetutils-ping smbclient php-libsmbclient \
+    php-smbclient coreutils php-ldap
+
+# Change the Document Root
+sed -i "s#html#owncloud#" /etc/apache2/sites-available/000-default.conf
+
+service apache2 restart
+
+FILE="/etc/apache2/sites-available/owncloud.conf"
+/bin/cat <<EOM >$FILE
+Alias /owncloud "/var/www/owncloud/"
+
+<Directory /var/www/owncloud/>
+  Options +FollowSymlinks
+  AllowOverride All
+
+ <IfModule mod_dav.c>
+  Dav off
+ </IfModule>
+
+ SetEnv HOME /var/www/owncloud
+ SetEnv HTTP_HOME /var/www/owncloud
+</Directory>
+EOM
+
+a2ensite owncloud.conf
+service apache2 reload
+
+
+#  Passwords
+dbPASS="$(</dev/urandom tr -dc 'A-Za-z0-9%&?@' |head -c 22 )"
+dbNAME="owncloud"
+dbUSER="owncloud"
+adminUSER="admin"
+adminPASS="$(</dev/urandom tr -dc 'A-Za-z0-9%&?@' |head -c 12 )"
+# ----------------------------
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${dbNAME}; \
+    GRANT ALL PRIVILEGES ON ${dbNAME}.* \
+    TO ${dbUSER}@localhost \
+    IDENTIFIED BY '${dbPASS}'";
+# ----------------------
+echo "Enabling Apache Modules"
+
+a2enmod dir env headers mime rewrite setenvif
+service apache2 reload
+cd /var/www/
+wget https://download.owncloud.org/community/owncloud-10.8.0.tar.bz2 && \
+    tar -xjf owncloud-10.8.0.tar.bz2 && \
+    chown -R www-data. owncloud
+# ----------------------------
+occ maintenance:install \
+    --database "mysql" \
+    --database-name "${dbNAME}" \
+    --database-user "${dbUSER}" \
+    --database-pass "${dbPASS}" \
+    --admin-user "${adminUSER}" \
+    --admin-pass "${adminPASS}"
+
+#occ config:system:get trusted_domains
+#occ config:system:set trusted_domains 1 --value="$myip"
+occ config:system:set trusted_domains 2 --value="$mydomain"
+
+# ----------------------
+occ background:cron
+echo "*/15  *  *  *  * /var/www/owncloud/occ system:cron" \
+    > /var/spool/cron/crontabs/www-data
+chown www-data.crontab /var/spool/cron/crontabs/www-data && chmod 0600 /var/spool/cron/crontabs/www-data
+
+# ----------------------
+echo "*/15 * * * * /var/www/owncloud/occ user:sync 'OCA\User_LDAP\User_Proxy' -m disable -vvv >> /var/log/ldap-sync/user-sync.log 2>&1" > /var/spool/cron/crontabs/www-data
+chown www-data.crontab  /var/spool/cron/crontabs/www-data && chmod 0600  /var/spool/cron/crontabs/www-data
+mkdir -p /var/log/ldap-sync && touch /var/log/ldap-sync/user-sync.log && chown www-data. /var/log/ldap-sync/user-sync.log
+
+# ----------------------
+occ config:system:set \
+   memcache.local \
+   --value '\OC\Memcache\APCu'
+
+occ config:system:set \
+   memcache.locking \
+   --value '\OC\Memcache\Redis'
+
+service redis-server start
+
+occ config:system:set \
+   redis \
+   --value '{"host": "127.0.0.1", "port": "6379"}' \
+   --type json
+
+# ----------------------
+FILE="/etc/logrotate.d/owncloud"
+sudo /bin/cat <<EOM >$FILE
+/var/www/owncloud/data/owncloud.log {
+  size 10M
+  rotate 12
+  copytruncate
+  missingok
+  compress
+  compresscmd /bin/gzip
+}
+EOM
+# ----------------------------
+cd /var/www/ 
+chown -R www-data. owncloud
+
+# ----------------------------
+touch "$HOME/.owncloud-profile.txt"
+echo -en "
+========   DATABASE  ========
+- OwnCloud site: ${mydomain}
+- DATABASE PASS: ${dbPASS}
+- DATABASE USER: ${dbUSER}
+- DATABASE NAME: ${dbNAME}
+========     USER    ========
+- Admin USER: ${adminUSER}
+- Admin PASS: ${adminPASS}
+=============================
+" >> "$HOME/.owncloud-profile.txt"
+# ----------------------------
+
+echo -en "ownCloud is now installed."
+echo -en "Use web browser to your ownCloud installation."
+# ----------------------------
+cat "$HOME/.owncloud-profile.txt"
+read -p "Press [Enter] key to continue..." fackEnterKey;
+
+}; 
+
+
+
+
+#--------------------------------------
+#      Owncloud Ubuntu 20
+#--------------------------------------
+function OwnCloud_V20() {
+# ----------------------------
+FILE="/usr/local/bin/occ"
+/bin/cat <<EOM >$FILE
+#! /bin/bash
+cd /var/www/owncloud
+sudo -E -u www-data /usr/bin/php /var/www/owncloud/occ "\$@"
+EOM
+chmod +x /usr/local/bin/occ
+# ----------------------------
+apt install -y \
+  apache2 wget openssl \
+  libapache2-mod-php \
+  mariadb-server \
+  php-imagick php-common php-curl php-gd php-imap php-intl \
+  php-json php-mbstring php-mysql php-ssh2 php-xml php-zip \
+  php-apcu php-redis redis-server \
+  
+# ----------------------------
+apt install -y ssh bzip2 rsync curl jq inetutils-ping coreutils
+
+
+# ============================
+#    Installation
+echo "Installation"
+sleep 3
+# ----------------------------
+sed -i "s#html#owncloud#" /etc/apache2/sites-available/000-default.conf
+service apache2 restart
+# ----------------------------
+FILE="/etc/apache2/sites-available/owncloud.conf"
+/bin/cat <<EOM >$FILE
+Alias /owncloud "/var/www/owncloud/"
+
+<Directory /var/www/owncloud/>
+  Options +FollowSymlinks
+  AllowOverride All
+
+ <IfModule mod_dav.c>
+  Dav off
+ </IfModule>
+
+ SetEnv HOME /var/www/owncloud
+ SetEnv HTTP_HOME /var/www/owncloud
+</Directory>
+EOM
+a2ensite owncloud.conf
+service apache2 reload
+
+# ============================
+#  Passwords
+# ============================
+dbPASS="$(</dev/urandom tr -dc 'A-Za-z0-9%&?@' |head -c 22 )"
+dbNAME="owncloud"
+dbUSER="owncloud"
+adminUSER="admin"
+adminPASS="$(</dev/urandom tr -dc 'A-Za-z0-9%&?@' |head -c 12 )"
+# ----------------------------
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${dbNAME}; \
+GRANT ALL PRIVILEGES ON ${dbNAME}.* \
+  TO ${dbUSER}@localhost \
+  IDENTIFIED BY '${dbPASS}'";
+# ----------------------------
+echo "Enabling Apache Modules"
+a2enmod dir env headers mime rewrite setenvif
+service apache2 reload
+# ----------------------------
+cd /var/www/
+wget https://download.owncloud.org/community/owncloud-10.8.0.tar.bz2 && \
+tar -xjf owncloud-10.8.0.tar.bz2 && \
+chown -R www-data. owncloud
+
+# ================================
+#    Install ownCloud
+# ================================
+echo "Installation OwnCloud"
+sleep 3
+# ----------------------------
+occ maintenance:install \
+    --database "mysql" \
+    --database-name "${dbNAME}" \
+    --database-user "${dbUSER}" \
+    --database-pass "${dbPASS}" \
+    --admin-user "${adminUSER}" \
+    --admin-pass "${adminPASS}"
+
+
+occ config:system:get trusted_domains
+occ config:system:set trusted_domains 1 --value="$myip"
+occ config:system:set trusted_domains 2 --value="$mydomain"
+occ config:system:get trusted_domains
+
+# ================================
+# sudo -u www-data php occ config:system:set trusted_domains 2 --value="$mydomain"
+#sudo nano /etc/apache2/sites-available/000-default.conf
+
+#ServerName cloud.example.com
+
+#ServerAdmin webmaster@localhost
+#DocumentRoot /var/www/owncloud/
+
+#ErrorLog ${APACHE_LOG_DIR}/error.log
+#CustomLog ${APACHE_LOG_DIR}/access.log combined
+# ================================
+
+# ----------------------------
+occ background:cron
+echo "*/15  *  *  *  * /var/www/owncloud/occ system:cron" \
+  > /var/spool/cron/crontabs/www-data
+chown www-data.crontab /var/spool/cron/crontabs/www-data
+chmod 0600 /var/spool/cron/crontabs/www-data
+# ----------------------------
+echo "
+# ==================   NOTE   ================== #
+- Если нужно синхронизировать пользователей с LDAP или 
+  Active Directory Server добавьте это дополнительное задание Cron.
+- Каждые 15 минут это задание cron будет синхронизировать пользов. 
+  LDAP в ownCloud и откл. тех, которые недоступны для ownCloud. 
+- Кроме того, получаете файл журнала отладки /var/log/ldap-sync/user-sync.log
+# ================================================ #
+"
+# ----------------------------
+echo "*/15 * * * * /var/www/owncloud/occ user:sync 'OCA\User_LDAP\User_Proxy' -m disable -vvv >> /var/log/ldap-sync/user-sync.log 2>&1" >> /var/spool/cron/crontabs/www-data
+chown www-data.crontab  /var/spool/cron/crontabs/www-data
+chmod 0600  /var/spool/cron/crontabs/www-data
+mkdir -p /var/log/ldap-sync
+touch /var/log/ldap-sync/user-sync.log
+chown www-data. /var/log/ldap-sync/user-sync.log
+# ----------------------------
+occ config:system:set \
+   memcache.local \
+   --value '\OC\Memcache\APCu'
+occ config:system:set \
+   memcache.locking \
+   --value '\OC\Memcache\Redis'
+occ config:system:set \
+   redis \
+   --value '{"host": "127.0.0.1", "port": "6379"}' \
+   --type json
+# ----------------------------
+FILE="/etc/logrotate.d/owncloud"
+sudo /bin/cat <<EOM >$FILE
+/var/www/owncloud/data/owncloud.log {
+  size 10M
+  rotate 12
+  copytruncate
+  missingok
+  compress
+  compresscmd /bin/gzip
+}
+EOM
+# ----------------------------
+  cd /var/www/
+  mkdir -p /var/www/owncloud && chown -R www-data. owncloud
+
+# ----------------------------
+  touch "$HOME/.owncloud-profile.txt"
+  echo -en "
+========   DATABASE  ========
+- OwnCloud site: ${mydomain}
+- DATABASE PASS: ${dbPASS}
+- DATABASE USER: ${dbUSER}
+- DATABASE NAME: ${dbNAME}
+========     USER    ========
+- Admin USER: ${adminUSER}
+- Admin PASS: ${adminPASS}
+=============================
+" >> "$HOME/.owncloud-profile.txt"
+# ----------------------------
+
+    echo -en "ownCloud is now installed."
+    echo -en "Use web browser to your ownCloud installation."
+    # ----------------------------
+    cat "$HOME/.owncloud-profile.txt"
+    read -p "Press [Enter] key to continue..." fackEnterKey;
+
+}
+
+
+# OwnCloud START Installation
+#----------------------------------------
+echo "Start Install ownCloud "
+read -p "Domain Name (eg. example.com): " mydomain
+sleep 3
+apt update && apt upgrade -y
+sleep 3
+
+  OS="$( cat /etc/*release |grep '^ID=' |sed 's/"//g' |awk -F= '{print $2 }' )";
+  release="$( cat /etc/*release |grep '^VERSION_ID=' |sed  's/"//g' |awk -F= '{print $2 }' )";
+  
+  if [[ "${OS}" == 'ubuntu' ]] && [[ "${release:0:2}" == 20 ]]; then
+    echo "Install OwnCloud for Ubuntu 20"
+    OwnCloud_V${release:0:2}
+  elif [[ "${OS}" == 'ubuntu' ]] && [[ "${release:0:2}" == 18 ]]; then
+    echo "Install OwnCloud for Ubuntu 18"
+    OwnCloud_V${release:0:2}
+  else
+    echo -en "\n ${RED} Not install OwnCloud.\n\n OS: ${OS}\n\n RELEASE: ${release} \n Run new installation script ${NC}\n"
+  fi;
+
 }
 
 
@@ -1958,18 +2358,19 @@ function installLAMP() {
 #==============================
 #           MENU
 #==============================
-function BOSSMENU() {
+function MAINMENU() {
 echo -e -n "\n\t${GREEN}${BGBlack}==== MAIN MENU ====${NC}\n"
 echo -e -n "${Yellow}
 \t1. Create SSH key     ${NC} ${Purple}
 \t2. Install LEMP       ${NC} ${BLUE}
-\t3. Install LAMP       ${NC} ${RED}
+\t3. Install LAMP       ${NC} ${Yellow}
+\t4. Control Panel      ${NC} ${RED}
 \n\tq. Quit...          ${NC}";
 
 }
 
-#   subMENU 1
-function SUBMENUONE() {
+#   Menu SSH
+function MenuSSH() {
 title="Generate Key SSH";
 echo -e -n "\n\t${GREEN}SSH KeyGen:${NC}\n"
 echo -e -n "
@@ -1993,17 +2394,26 @@ echo -e -n "
 \t4. Install WordPress ${CYAN} With All Services Cloudflare ${NC}
 \t5. Instal WordPress ${CYAN} With All Services Certbot ${NC}
 ${RED}\n\t0. Back ${NC}\n";
-
 }
 
 ##   subMENU 3
 function MenuLAMP() {
-        echo -e "\n\t ${GREEN}SubMENU 3 OPTIONS:${NC} \n"
-        echo -e -n "${Yellow}
+echo -e "\n\t ${GREEN}LAMP installation & Settings:${NC} \n"
+echo -e -n "${Yellow}
 \t1. Install LAMP with WordPress
 \t\t ${CYAN}(Apache, php7.4, phpMyAdmin with WordPress) ${NC} ${RED}
 \n\t0. Back ${NC}\n";
+}
 
+function MenuCONTROLPANEL() {
+  echo -e "\n\t ${GREEN}MAIN MENU 4 OPTIONS:${NC} \n"
+  echo -e -n "${Yellow}
+\t1. Install OwnCloud      ${NC} ${RED}
+\t2. Install               ${NC} ${RED}
+\t3. Install               ${NC} ${RED}
+\t4. Install               ${NC} ${RED}
+\n\t0. Back                ${NC}\n
+";
 }
 
 
@@ -2011,7 +2421,7 @@ function MenuLAMP() {
 while :
 do
         showBanner
-        BOSSMENU
+        MAINMENU
         echo -n -e "\n\tSelection: "
         read -n1 opt
         a=true;
@@ -2022,7 +2432,7 @@ do
                 while :
                 do
                         showBanner
-                        SUBMENUONE
+                        MenuSSH
                         echo -n -e "\n\tSelection: "
                         read -n1 opt;
                         case $opt in
@@ -2075,6 +2485,27 @@ do
                         esac
                 done
                 ;;
+# 2 ----------------------------
+                4) echo -e "Control Panell: "
+                while :
+                do
+                        showBanner
+                        MenuCONTROLPANEL
+                        echo -n -e "\n\tSelection: "
+                        read -n1 opt;
+                        case $opt in
+                                1) OWNCLOUD ;;
+                                2) echo -e "FREE $opt" ;;
+                                3) echo -e "FREE $opt"  ;;
+                                4) echo -e "FREE $opt"  ;;
+                                5) echo -e "FREE $opt"  ;;
+                                /q | q | 0) break ;;
+                                *) ;;
+                        esac
+                done
+                ;;
+
+# 3 ----------------------------
 
                 /q | q | 0) echo; break ;;
                 *) ;;
