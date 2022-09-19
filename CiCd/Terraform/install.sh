dcacfc1299dbab888c9bbe204f3048c6679acdf6
GitKitNet <101803117+GitKitNet@users.noreply.github.com>
2022-09-19 11:45:35 +0300

    Update install.sh

--- CiCd/Terraform/install.sh
+++ CiCd/Terraform/install.sh
@@ -43,6 +43,26 @@ cd /opt/terraform/yandex
 
 echo "Мы готовы приступить непосредственно к работе с terraform."
 
+#-------
+
+main.tf
+terraform {
+  required_version = "= 1.1.7"
+
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "= 0.73"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = "<OAuth>"
+  cloud_id  = "<идентификатор облака>"
+  folder_id = "<идентификатор каталога>"
+  zone      = "<зона доступности по умолчанию>"
+}
 
 
 #--------------------
