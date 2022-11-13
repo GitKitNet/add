4826b81eaeb0527cf465afd3a223e54878f08e4a
GitKitNet <101803117+GitKitNet@users.noreply.github.com>
2022-11-13 04:00:24 +0200

    Update openvpn-install.sh

--- ToolKit/OpenVPN/openvpn-install.sh
+++ ToolKit/OpenVPN/openvpn-install.sh
@@ -124,7 +124,7 @@ if [[ ! -e /etc/openvpn/server/server.conf ]]; then
 		[[ -z "$ip_number" ]] && ip_number="1"
 		ip=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | sed -n "$ip_number"p)
 	fi
-	# If $ip is a private IP address, the server must be behind NAT
+	# If $ip is a private IP address, the server must be behind NAT
 	if echo "$ip" | grep -qE '^(10\.|172\.1[6789]\.|172\.2[0-9]\.|172\.3[01]\.|192\.168)'; then
 		echo
 		echo "This server is behind NAT. What is the public IPv4 address or hostname?"
@@ -564,6 +564,7 @@ else
 			exit
 		;;
 	esac
-fi;
+fi
 
-## END #########
+
+## END ##############
